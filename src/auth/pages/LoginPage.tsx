f4634b86165178a5b0e391b15aa2d02ae25d9739
Camilo López <camiloj.lopez79@gmail.com>
2022-08-18 12:29:45 -0400

    Implemented styles for Auth layout and Auth pages

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,5 +1,72 @@
+import { Link as RouterLink } from 'react-router-dom';
+// Material UI
+import {
+  Button,
+  Grid,
+  Link,
+  TextField,
+} from '@mui/material';
+// Layout
+import { AuthLayout } from '../layout';
+
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <AuthLayout title='Login'>
+
+      <form>
+        <Grid container>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Email'
+              type='email'
+              placeholder='What is your email?' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Password'
+              type='password'
+              placeholder='What is your password?' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid
+            container
+            spacing={ 2 }
+            sx={{
+              mb: 2,
+              mt: 2
+            }}
+          >
+            <Grid item xs={ 12 }>
+              <Button
+                variant='contained'
+                fullWidth
+              >
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid
+            container
+            direction='row'
+            justifyContent='end'
+          >
+            <Link
+              component={ RouterLink }
+              color='inherit'
+              to='/auth/register'
+            >
+              Create an account
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+
+    </AuthLayout>
   )
 }
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,6 +1,82 @@
+import { Link as RouterLink } from 'react-router-dom';
+// Material UI
+import {
+  Button,
+  Grid,
+  Link,
+  TextField
+} from '@mui/material';
+// Layout
+import { AuthLayout } from '../layout';
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+
+      <form>
+        <Grid container>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Name'
+              type='text'
+              placeholder='What is your name?' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Email'
+              type='email'
+              placeholder='What is your email?' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Password'
+              type='password'
+              placeholder='What is your password?' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid
+            container
+            spacing={ 2 }
+            sx={{
+              mb: 2,
+              mt: 2
+            }}
+          >
+            <Grid item xs={ 12 }>
+              <Button
+                variant='contained'
+                fullWidth
+              >
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid
+            container
+            direction='row'
+            justifyContent='end'
+          >
+            <Link
+              component={ RouterLink }
+              color='inherit'
+              to='/auth/login'
+            >
+              You already have an account?
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+
+    </AuthLayout>
   )
 }
 
